ca9d598779ad20212bec16a8c28d939ed8abfde2
Kseniia <ivankovaks@gmail.com>
2025-02-07 15:46:08 +0100

    избавляемся от render.js. Применяем паттерн observe

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+	console.log('State changed');
+}
 
 let state = {
 		profilePage: {
@@ -35,7 +37,7 @@ let state = {
 		}
 };
 
-export let addPost = () => {
+export const addPost = () => {
 	let newPost = {
 		id: 4,
 		message: state.profilePage.newPostText,
@@ -47,7 +49,7 @@ export let addPost = () => {
 	rerenderEntireTree(state);
 }
 
-export let addMessage = () => {
+export const addMessage = () => {
 	let newMessage = {
 		id: 5,
 		message: state.dialogsPage.newMessageText
@@ -58,14 +60,18 @@ export let addMessage = () => {
 	rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 	state.profilePage.newPostText = newText;
 	rerenderEntireTree(state);
 }
 
-export let updateNewMessageText = (newText) => {
+export const updateNewMessageText = (newText) => {
 	state.dialogsPage.newMessageText = newText;
 	rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+	rerenderEntireTree = observer;
 }
 
 export default state;
